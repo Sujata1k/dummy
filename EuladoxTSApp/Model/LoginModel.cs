f5d4cd82ec628991595779e3e3b057df9a5b1022
Sujata1k <48507540+Sujata1k@users.noreply.github.com>
2020-04-29 00:33:46 +0530

    The project working

---
+++ EuladoxTSApp/Model/LoginModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+namespace EuladoxTSApp.Model
+{
+    public class LoginModel
+    {
+        public int id_eladox_user { get; set; }
+
+        public string Agent { get; set; }
+
+        public string user_name { get; set; }
+
+        public string user_password { get; set; }
+
+        public string caller_type { get; set; }
+
+        public string client_host_name { get; set; }
+
+        public string client_serial_number { get; set; }
+
+        public string session_key { get; set; }
+
+        public string key_valid_till { get; set; }
+
+        public bool IsLoginCredenntialValid { get; set; }
+
+        public bool RetsultHasCriticalError { get; set; }
+
+        public string ErrorMessage { get; set; }
+        public string login_by { get; set; }
+
+    
+}
+}
---
+++ EuladoxTSApp/View/LoginPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using EuladoxTSApp.View;
+using EuladoxTSApp.Model;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace EuladoxTSApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Clicked(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client service1Client = new ServiceReference1.Service1Client();
+            LoginModel LoginResponseObject = new LoginModel();
+
+            LoginModel LoginRequestObject = new LoginModel()
+            {
+                user_name = EntUserName.Text,
+                user_password = EntPassword.Text,
+                client_host_name = "Sujata",
+                client_serial_number = "SK12345678",
+                caller_type = "MOBAPPAPI",
+                login_by = "Agent"
+            };
+            string LoginJson = Newtonsoft.Json.JsonConvert.SerializeObject(LoginRequestObject);
+            try
+            {       
+                var result = client.fnValidateLoginCredential(LoginJson);
+                LoginResponseObject = JsonConvert.DeserializeObject<LoginModel>(result);
+            //    bool test = false;
+            }
+            catch (Exception ex)
+            {
+               // bool test1 = false;
+            }
+            // var json = JsonConvert.SerializeObject(Login);
+
+
+        }
+    }
+}
